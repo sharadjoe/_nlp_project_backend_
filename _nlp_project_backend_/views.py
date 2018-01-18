7110dec842dc01f23a7ce2dea9019968121813ef
sharad joe <sharadjoe48@gmail.com>
2018-01-18 13:39:16 +0530

    Just created the new rake file
    to test django in

--- _nlp_project_backend_/views.py
+++ _nlp_project_backend_/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 from analysis.models import textarea
 
+
+
 def home(request):
     return render(request,"index.html")
 
 
 def try_it(request):
+    
     return render(request,"try_it.html")
+
+
+
 
 def try1(request):
     return render(request,"try1.html")
